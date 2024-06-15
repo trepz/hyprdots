837b4b0cc16b7c61feb5cbfeacb7bcf5d20e1d66
trepz <tr3ppy@gmail.com>
2024-06-16 00:16:03 +1000

    Some neovim core things to get going

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,4 @@
+return {
+  'tpope/vim-sleuth',
+  'nvim-lua/plenary.nvim',
+}
---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
+
+    -- Document existing key chains
+    require('which-key').register {
+      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
+    }
+    -- visual mode
+    require('which-key').register({
+      ['<leader>h'] = { 'Git [H]unk' },
+    }, { mode = 'v' })
+  end,
+}
