af3243095228cadc32e72c35fbd352ae4c7bec6f
trepz <tr3ppy@gmail.com>
2024-06-17 06:49:57 +1000

    Change inlay hints bind

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -59,9 +59,9 @@ return { -- LSP Configuration & Plugins
         --
         -- This may be unwanted, since they displace some of your code
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+          map('<leader>lh', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+          end, 'toggle inlay hints')
         end
       end,
     })
