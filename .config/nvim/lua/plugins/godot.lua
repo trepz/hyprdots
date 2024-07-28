a56772257fee3cb766a284c32c3d3a6bab8e9eaf
trepz <tr3ppy@gmail.com>
2024-07-28 16:58:54 +1000

    Fix vim godot lsp integration

--- .config/nvim/lua/plugins/godot.lua
+++ .config/nvim/lua/plugins/godot.lua
@@ -1,3 +1,11 @@
 return {
   'habamax/vim-godot',
+  dependencies = { 'neovim/nvim-lspconfig' },
+  config = function()
+    require('lspconfig').gdscript.setup {
+      flags = {
+        debounce_text_changes = 150,
+      },
+    }
+  end,
 }
