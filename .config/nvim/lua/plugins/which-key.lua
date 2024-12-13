4ac3aefea2ca5f5ecc04c3954531bba5b0ec8b9b
trepz <tr3ppy@gmail.com>
2024-12-13 21:28:56 +1000

    Update which key use

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,18 +2,16 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
-      ['<leader>l'] = { name = 'LSP', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = 'Search', _ = 'which_key_ignore' },
-      ['<leader>y'] = { name = 'Yank', _ = 'which_key_ignore' },
+    require('which-key').setup {
+      { '<leader>g', group = 'Git' },
+      { '<leader>g_', hidden = true },
+      { '<leader>l', group = 'LSP' },
+      { '<leader>l_', hidden = true },
+      { '<leader>s', group = 'Search' },
+      { '<leader>s_', hidden = true },
+      { '<leader>y', group = 'Yank' },
+      { '<leader>y_', hidden = true },
+      { '<leader>h', desc = 'Git hunk', mode = 'v' },
     }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git hunk' },
-    }, { mode = 'v' })
   end,
 }
