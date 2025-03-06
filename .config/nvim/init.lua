ec16f24b6c1a99c4bdc595a48c6144dae7088f6a
trepz <tr3ppy@gmail.com>
2025-03-06 23:17:12 +1000

    Harpoon at home

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,6 +40,18 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<Leader>yl', ":call setreg('+', expand('%:.') .. ':' .. line('.'))<CR>", { desc = 'Yank file path and line number' })
 vim.keymap.set('n', '<Leader>yp', ":call setreg('+', expand('%:.'))<CR>", { desc = 'Yank file path' })
 
+-- We have harpoon at home
+vim.keymap.set('n', '<Leader>t', function()
+  local char = vim.fn.getcharstr()
+  local mark = string.upper(char)
+  local _, _, b, n = unpack(vim.api.nvim_get_mark(mark, {}))
+  local buf = b
+  if not (b > 0) then
+    buf = vim.fn.bufadd(n)
+  end
+  vim.cmd('b' .. buf)
+end)
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
