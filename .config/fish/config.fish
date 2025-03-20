ecd5996779faf4985b2461f69273574a31182720
trepz <tr3ppy@gmail.com>
2025-03-21 09:05:56 +1000

    Let opam touch my fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,12 @@ if status is-interactive
         nvm use lts
     end
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/trepz/.opam/opam-init/init.fish' && source '/home/trepz/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
